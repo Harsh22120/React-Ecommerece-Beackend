b7e3ab97d1c2c72c23753cf0ee7f350cdd846c2d
unknown <gajeraharsh52438@gmail.com>
2022-03-10 15:26:27 +0530

    add all my files

---
+++ index.js
@@ -0,0 +1,200 @@
+const express=require('express');
+const mongoose= require('mongoose');
+
+const bodyparser=require('body-parser');
+const cookieParser=require('cookie-parser');
+const User = require('./models/user');
+const Product = require('./models/Product');
+const Cart = require("./models/cart");
+const {auth} =require('./middlewares/auth');
+const req = require('express/lib/request');
+const db=require('./config/config').get(process.env.NODE_ENV);
+
+const app=express();
+// app use
+app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.json());
+app.use(cookieParser());
+
+// database connection
+mongoose.Promise=global.Promise;
+mongoose.connect(db.DATABASE,{ useNewUrlParser: true,useUnifiedTopology:true },function(err){
+    if(err) console.log(err);
+    console.log("database is connected");
+});
+
+//product
+app.get("/api/products", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//cart
+//post cart
+app.post("/api/carts/:id", async (req, res) => {
+  const { productId, quantity, name, price } = req.body;
+  const userId = req.params.id; //TODO: the logged in user id
+
+  try {
+    let cart = await Cart.findOne({ userId });
+
+    if (cart) {
+      //cart exists for user
+      let itemIndex = cart.products.findIndex(p => p.productId == productId);
+
+      if (itemIndex > -1) {
+        //product exists in the cart, update the quantity
+        let productItem = cart.products[itemIndex];
+        productItem.quantity = quantity;
+        cart.products[itemIndex] = productItem;
+      } else {
+        //product does not exists in cart, add new item
+        cart.products.push({ productId, quantity, name, price });
+      }
+      cart = await cart.save();
+      return res.status(201).send(cart);
+    } else {
+      //no cart for user, create new cart
+      const newCart = await Cart.create({
+        userId,
+        products: [{ productId, quantity, name, price }]
+      });
+      
+      /* const carts = newCart.save().then((res)=>{
+        res.send(res)
+      }).catch(err)({req})
+      */
+      return res.status(201).send(newCart);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Something went wrong");
+  }
+});
+
+//GET USER CART
+app.get("/find/:userId", async (req, res) => {
+  try {
+    const cart = await Cart.findOne({ userId: req.params.userId });
+    res.status(200).json(cart);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //GET ALL
+app.get("/carts", async (req, res) => {
+  try {
+    const carts = await carts.find();
+    res.message("my cart data").status(200).json( carts );
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+
+// adding new user (sign-up route)
+app.post('/api/register',function(req,res){
+   // taking a user
+   const newuser=new User(req.body);
+   console.log(newuser);
+
+   if(newuser.password!=newuser.password2)return res.status(400).json({message: "password not match"});
+   
+   User.findOne({email:newuser.email},function(err,user){
+       if(user) return res.status(400).json({ auth : false, message :"email exits"});
+      
+       newuser.save((err,doc)=>{
+           if(err) {console.log(err);
+               return res.status(400).json({ success : false});}
+           res.status(200).json({
+               succes:true,
+               user : doc
+           });
+       });
+   });
+});
+
+
+// login user
+app.post('/api/login', function(req,res){
+    let token=req.cookies.auth;
+    User.findByToken(token,(err,user)=>{
+        if(err) return  res(err);
+        if(user) return res.status(400).json({
+            error :true,
+            message:"You are already logged in"
+        });
+    
+        else{
+            User.findOne({'email':req.body.email},function(err,user){
+                if(!user) return res.json({isAuth : false, message : ' Auth failed ,email not found'});
+        
+                user.comparepassword(req.body.password,(err,isMatch)=>{
+                    if(!isMatch) return res.json({ isAuth : false,message : "password doesn't match"});
+        
+                user.generateToken((err,user)=>{
+                    if(err) return res.status(400).send(err);
+                    res.cookie('auth',user.token).json({
+                        isAuth : true,
+                        id : user._id
+                        ,email : user.email
+                    });
+                });    
+            });
+          });
+        }
+    });
+});
+
+//logout user
+ app.get('/api/logout',auth,function(req,res){
+        req.user.deleteToken(req.token,(err,user)=>{
+            if(err) return res.status(400).send(err);
+            res.sendStatus(200);
+        });
+
+    }); 
+
+// get logged in user
+app.get('/api/profile',auth,function(req,res){
+        res.json({
+            isAuth: true,
+            id: req.user._id,
+            email: req.user.email,
+            name: req.user.firstname + req.user.lastname
+            
+        })
+});
+
+
+app.get('/',function(req,res){
+    res.status(200).send(`Welcome to login , sign-up api`);
+});
+
+// listening port
+const PORT=process.env.PORT||3000;
+app.listen(PORT,()=>{
+    console.log(`app is live at ${PORT}`);
+});
